eee5c45ac07a226d17b12749603df7863ba9df34
Paweł Bogdan <pawel.bogdan1@gmail.com>
2019-10-26 16:40:35 +0200

    Zarys zadania z kontami bankowymi

---
+++ src/pl/altkom/accounts/Client.java
@@ -0,0 +1,10 @@
+package pl.altkom.accounts;
+
+public class Client {
+    public static void main(String[] args) {
+        String srcAccount = "1111111";
+        String destAccount = "2222222";
+        double cash = 10000;
+
+    }
+}
